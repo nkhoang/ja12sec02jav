c65a69f3d3fbf193ac14aea4a686c68618283988
Hoang Khanh Nguyen <hoangknguyen.it@gmail.com>
2010-11-09 03:24:22 +0000

    - Added Zoom Line chart. - Great chart ever. Will apply it soon.

--- run3.bat
+++ run3.bat
@@ -1,4 +1,4 @@
 #copy "%JAVA_HOME%\lib\tools.jar" "E:\Softs\appengine-java-sdk-1.3.7\lib\shared"
 call mvn clean
 call mvn package -o -Dmaven.test.skip=true
-call "F:\Softs\Softs\Development\Java\Runtime\appengine-java-sdk-1.3.7\bin\dev_appserver.cmd" --disable_update_check "%~dp0target\ShopNow-1.0"
+call "E:\Softs\appengine-java-sdk-1.3.7\bin\dev_appserver.cmd" --disable_update_check "%~dp0target\ShopNow-1.0"
