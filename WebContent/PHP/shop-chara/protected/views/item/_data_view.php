80932fc2933caa08caad4684cf1c3627b27f714e
Hoang Khanh Nguyen <hoangknguyen.it@gmail.com>
2010-12-21 09:34:05 +0000

    - GUI update for item category view.

--- WebContent/PHP/shop-chara/protected/views/item/_data_view.php
+++ WebContent/PHP/shop-chara/protected/views/item/_data_view.php
@@ -1,7 +1,14 @@
 
 <div class="ic">
-    <div class="img_c">
+    <div class="img_c it_b">
         <!-- may check image existence here -->
         <img height="40"  src="<?php echo $data->itemPictures[0]->link ?>"/>
     </div>
+
+    <div class="it_b">
+        <div class="it_title"><?php echo CHtml::encode($data->getAttributeLabel('item_id')); ?></div>
+        <div class="seperator"></div>
+        <div class="it_content"><?php echo CHtml::encode($data->item_id); ?></div>
+    </div>
+         
 </div>
