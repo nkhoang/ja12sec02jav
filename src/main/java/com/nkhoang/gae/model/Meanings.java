bc29047f1bec3ccb1ef75d7473b05c343ecac84a
Hoang Khanh Nguyen <hoangknguyen.it@gmail.com>
2011-06-02 09:28:06 +0000

    - Spring multiple configuration files detection fixed. - Code optimized and reorganized.

---
+++ src/main/java/com/nkhoang/gae/model/Meanings.java
@@ -0,0 +1,19 @@
+package com.nkhoang.gae.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Meanings {
+	private List<Meaning> meanings = new ArrayList<Meaning>();
+
+	public List<Meaning> getMeanings() {
+		return meanings;
+	}
+
+	public void setMeanings(List<Meaning> meanings) {
+		this.meanings = meanings;
+	}
+
+
+}
