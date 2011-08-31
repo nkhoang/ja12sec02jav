3ddfdcdd4643af682add54fc62cbcf14ab20f922
Hoang Khanh Nguyen <hoangknguyen.it@gmail.com>
2011-08-31 06:44:25 +0000

    - Remove everything in order to reduce total files count.

--- src/main/java/com/nkhoang/common/db/SerializableResultSetMetaData.java
+++
@@ -1,8 +0,0 @@
-package com.nkhoang.common.db;
-
-import java.io.Serializable;
-import java.sql.ResultSetMetaData;
-
-/** This is an empty interface that extends ResultSetMetaData and Serializable. */
-public interface SerializableResultSetMetaData extends ResultSetMetaData, Serializable {
-}
