fdd1b2843529d45b1ffee0634bddcafcbc49e095
Hoang Khanh Nguyen <hoangknguyen.it@gmail.com>
2011-08-31 09:26:48 +0000

    - Add some unit tests back to place.

---
+++ util/src/test/java/com/nkhoang/comparator/ComparatorTest.java
@@ -0,0 +1,62 @@
+package com.nkhoang.comparator;
+
+import org.apache.commons.lang.math.NumberUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+public class ComparatorTest {
+	private static final Logger LOG = LoggerFactory.getLogger(ComparatorTest.class.getCanonicalName());
+
+	@Test
+	public void testComparator() {
+		List<Person> persons = new ArrayList<Person>();
+		Person p = new Person(1);
+		Person p2 = new Person(3);
+		Person p3 = new Person(12);
+
+		persons.add(p3);
+		persons.add(p);
+		persons.add(p2);
+
+		LOG.info(persons.toString());
+
+		Collections.sort(
+			persons, new Comparator<Person>() {
+
+			public int compare(Person o1, Person o2) {
+				return NumberUtils.compare(o1.getRank(), o2.getRank());
+			}
+		});
+		LOG.info(persons.toString());
+
+		persons = persons.subList(0, 2);
+		LOG.info(persons.toString());
+	}
+
+	private class Person {
+		private int _rank;
+
+		public Person(int rank) {
+			_rank = rank;
+		}
+
+		public int getRank() {
+			return _rank;
+		}
+
+		public void setRank(int rank) {
+			_rank = rank;
+		}
+
+		public String toString() {
+			return String.format("P[%s]", _rank);
+		}
+	}
+}
--- util/src/test/java/com/nkhoang/math/PRNUtilTest.java
+++ util/src/test/java/com/nkhoang/math/PRNUtilTest.java
@@ -1,16 +1,20 @@
 package com.nkhoang.math;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class PRNUtilTest {
+	private static final Logger LOG = LoggerFactory.getLogger(PRNUtilTest.class.getCanonicalName());
+
 	@Test
 	public void testPRN() {
 
 		String i = " A AND b and C or D Or F";
 		i = i.replaceAll("AND|And|and", "&");
 		i = i.replaceAll("OR|Or|or", "|");
-		System.out.println(i);
+		LOG.info(i);
 
 		String s = "(Lastname      & FirstName) | City & &";
 		String s2 = " Lastname & (FirstName | City City & Name) Firstname &";
@@ -23,11 +27,11 @@ public class PRNUtilTest {
 		String s9 = "A & B | C";
 		String s10 = "(A & B) | (B & D) & (C | F)";
 
-		System.out.println(PRNUtil.checkPRNExpression(PRNUtil.Infix2(s10)));
-		System.out.println(PRNUtil.checkPRNExpression(PRNUtil.Infix2(s8)));
-		System.out.println(PRNUtil.checkPRNExpression(PRNUtil.Infix2(s9)));
-		//System.out.println(PRNUtil.checkPRNExpression(PRNUtil.Infix2(s2)));
-		//System.out.println(PRNUtil.checkPRNExpression(PRNUtil.Infix2(s3)));
-		//System.out.println(PRNUtil.checkPRNExpression(PRNUtil.Infix2(s4)));
+		LOG.info(Boolean.toString(PRNUtil.checkPRNExpression(PRNUtil.Infix2(s10))));
+		LOG.info(Boolean.toString(PRNUtil.checkPRNExpression(PRNUtil.Infix2(s8))));
+		LOG.info(Boolean.toString(PRNUtil.checkPRNExpression(PRNUtil.Infix2(s9))));
+		LOG.info(Boolean.toString(PRNUtil.checkPRNExpression(PRNUtil.Infix2(s2))));
+		LOG.info(Boolean.toString(PRNUtil.checkPRNExpression(PRNUtil.Infix2(s3))));
+		LOG.info(Boolean.toString(PRNUtil.checkPRNExpression(PRNUtil.Infix2(s4))));
 	}
 }
