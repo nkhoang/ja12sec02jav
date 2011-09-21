d19af8df1774307dd19f84f69393291ef381780d
Hoang Khanh Nguyen <hoangknguyen.it@gmail.com>
2011-09-21 15:07:40 +0000

    - Totally remove Lucene project from project list.

--- lucene/src/main/java/com/nkhoang/search/LuceneSearchFields.java
+++
@@ -1,7 +0,0 @@
-package com.nkhoang.search;
-
-public class LuceneSearchFields {
-	public static final String ID = "id";
-	public static final String  WORD_DESCRIPTION = "description";
-	public static final String WORD_CONTENT = "content";
-}
