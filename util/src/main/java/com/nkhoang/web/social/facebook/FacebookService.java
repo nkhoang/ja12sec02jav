f22910fd034372b7f9f0b5a68e6d59d4bb2a345a
Hoang Khanh Nguyen <hoangknguyen.it@gmail.com>
2011-10-13 04:28:52 +0000

    - Working with HTML5 canvas. - Facebook service added.

---
+++ util/src/main/java/com/nkhoang/web/social/facebook/FacebookService.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.nkhoang.web.social.facebook;
+
+/**
+ * <PRE>
+ * Facebook functions service.
+ * </PRE>
+ * 
+ * @version 1.0.0
+ */
+public interface FacebookService {
+
+	/**
+	 * Post a new content
+	 * 
+	 * @param caption caption.
+	 * @param description description.
+	 * @param imgURL image url.
+	 * @param name name.
+	 * @param linkToImg a link to image.
+     * @param linkToWebsite a linke to website.
+     * @return returned String.	 
+	 */
+	public String postContent(
+		String caption, String description, String imgURL, String name, String linkToImg, String linkToWebsite);
+
+	public boolean removePost(String postID);
+
+}
