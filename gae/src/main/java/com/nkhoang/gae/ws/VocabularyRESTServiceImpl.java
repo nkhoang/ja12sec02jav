1e4df71cd08b22c4b9a3d5cdee854185c2316ec0
Hoang Khanh Nguyen <hoangknguyen.it@gmail.com>
2011-11-02 23:59:25 +0000

    = Need to investigate why REST is not working

--- gae/src/main/java/com/nkhoang/gae/ws/VocabularyRESTServiceImpl.java
+++ gae/src/main/java/com/nkhoang/gae/ws/VocabularyRESTServiceImpl.java
@@ -22,102 +22,101 @@ import java.util.List;
 @Service
 @Path("/")
 public class VocabularyRESTServiceImpl {
-	private static Logger LOG = LoggerFactory.getLogger(VocabularyRESTServiceImpl.class.getCanonicalName());
-	private VocabularyService vocabularyService;
-	private VocabularyDao     vocabularyDao;
-	private WordLuceneDao     wordLuceneDao;
+    private static Logger LOG = LoggerFactory.getLogger(VocabularyRESTServiceImpl.class.getCanonicalName());
+    private VocabularyService vocabularyService;
+    private VocabularyDao vocabularyDao;
+    private WordLuceneDao wordLuceneDao;
+
 
     @GET
-	@Produces("application/xml")
-	@Path("/search/{word}")
-	public Word search(@PathParam("word") String word) {
-		Word result = null;
-		try {
-			result = vocabularyService.save(word);
-		}
-		catch (Exception ex) {
-			LOG.error(String.format("WS could not lookup: ", word), ex);
-		}
+    @Produces("application/xml")
+    @Path("/search/{word}")
+    public Word search(@PathParam("word") String word) {
+        Word result = null;
+        try {
+            result = vocabularyService.save(word);
+        } catch (Exception ex) {
+            LOG.error(String.format("WS could not lookup: ", word), ex);
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	@GET
-	@Produces("application/xml")
-	@Path("/search/id/{id}")
-	public Word searchById(@PathParam("id") Long id) {
-		Word result = vocabularyDao.get(id);
-		if (result == null) {
-			result = new Word();
-		}
+    @GET
+    @Produces("application/xml")
+    @Path("/search/id/{id}")
+    public Word searchById(@PathParam("id") Long id) {
+        Word result = vocabularyDao.get(id);
+        if (result == null) {
+            result = new Word();
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	@GET
-	@Produces("application/json")
-	@Path("/lucene/getAll")
-	public String getAllLuceneWords() {
-		List<WordLucene> wordLucenes = wordLuceneDao.getAll();
-		List<String> results = new ArrayList<String>();
-		try {
-			JAXBContext context = JAXBContext.newInstance(WordLucene.class);
-			Marshaller marshaller= context.createMarshaller();
+    @GET
+    @Produces("application/json")
+    @Path("/lucene/getAll")
+    public String getAllLuceneWords() {
+        List<WordLucene> wordLucenes = wordLuceneDao.getAll();
+        List<String> results = new ArrayList<String>();
+        try {
+            JAXBContext context = JAXBContext.newInstance(WordLucene.class);
+            Marshaller marshaller = context.createMarshaller();
 
-			if (CollectionUtils.isNotEmpty(wordLucenes)) {
-				for (WordLucene wl : wordLucenes) {
-					StringWriter writer = new StringWriter();
-					marshaller.marshal(wl, writer);
-					results.add(writer.toString());
-				}
-			}
-		}
-		catch (JAXBException jaxbe) {
-			LOG.error("Could not initialize JAXBContext.", jaxbe);
-		}
-		Gson gson = new Gson();
-		return gson.toJson(results);
-	}
+            if (CollectionUtils.isNotEmpty(wordLucenes)) {
+                for (WordLucene wl : wordLucenes) {
+                    StringWriter writer = new StringWriter();
+                    marshaller.marshal(wl, writer);
+                    results.add(writer.toString());
+                }
+            }
+        } catch (JAXBException jaxbe) {
+            LOG.error("Could not initialize JAXBContext.", jaxbe);
+        }
+        Gson gson = new Gson();
+        return gson.toJson(results);
+    }
 
-	@DELETE
-	@Consumes("text/plain")
-	@Path("/lucene/delete/{id}")
-	public void deleteLuceneWord(@PathParam("id") Long id) {
-		wordLuceneDao.delete(id);
-	}
+    @DELETE
+    @Consumes("text/plain")
+    @Path("/lucene/delete/{id}")
+    public void deleteLuceneWord(@PathParam("id") Long id) {
+        wordLuceneDao.delete(id);
+    }
 
-	@GET
-	@Produces("application/json")
-	@Path("/getAll")
-	public String getAllWords(@QueryParam("offset") int offset, @QueryParam("size") int size, @QueryParam("direction") String direction) {
-		List<Word> words = vocabularyService.getAllWordsByRange(offset, size, direction, false);
-		List<String> wordStrings = new ArrayList<String>();
-		for (Word w : words) {
-			wordStrings.add(w.getDescription());
-		}
+    @GET
+    @Produces("application/json")
+    @Path("/getAll")
+    public String getAllWords(@QueryParam("offset") int offset, @QueryParam("size") int size, @QueryParam("direction") String direction) {
+        List<Word> words = vocabularyService.getAllWordsByRange(offset, size, direction, false);
+        List<String> wordStrings = new ArrayList<String>();
+        for (Word w : words) {
+            wordStrings.add(w.getDescription());
+        }
 
-		Gson gson = new Gson();
-		return gson.toJson(wordStrings);
-	}
+        Gson gson = new Gson();
+        return gson.toJson(wordStrings);
+    }
 
-	public VocabularyService getVocabularyService() {
-		return vocabularyService;
-	}
+    public VocabularyService getVocabularyService() {
+        return vocabularyService;
+    }
 
 
-	public void setVocabularyService(VocabularyService vocabularyService) {
-		this.vocabularyService = vocabularyService;
-	}
+    public void setVocabularyService(VocabularyService vocabularyService) {
+        this.vocabularyService = vocabularyService;
+    }
 
-	public VocabularyDao getVocabularyDao() {
-		return vocabularyDao;
-	}
+    public VocabularyDao getVocabularyDao() {
+        return vocabularyDao;
+    }
 
-	public void setVocabularyDao(VocabularyDao vocabularyDao) {
-		this.vocabularyDao = vocabularyDao;
-	}
+    public void setVocabularyDao(VocabularyDao vocabularyDao) {
+        this.vocabularyDao = vocabularyDao;
+    }
 
-	public void setWordLuceneDao(WordLuceneDao wordLuceneDao) {
-		this.wordLuceneDao = wordLuceneDao;
-	}
+    public void setWordLuceneDao(WordLuceneDao wordLuceneDao) {
+        this.wordLuceneDao = wordLuceneDao;
+    }
 }
