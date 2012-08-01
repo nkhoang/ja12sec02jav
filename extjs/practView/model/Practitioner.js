0b7d712733b486731c3b4ba6fd95f09d850976ad
Hoang Khanh Nguyen <hoangknguyen.it@gmail.com>
2012-08-01 12:36:31 +0000

    - Remove all and prepare for a new one

--- extjs/practView/model/Practitioner.js
+++
@@ -1,6 +0,0 @@
-Ext.define('practView.model.Practitioner', {
-  extend:'Ext.data.Model',
-  fields:[
-    'name', 'npi', 'type', 'address', 'status', 'licenseState', 'vendible', 'numAffil'
-  ]
-});
